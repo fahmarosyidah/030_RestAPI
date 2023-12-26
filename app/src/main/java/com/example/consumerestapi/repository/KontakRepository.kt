9e988245deb116d8ad32b945321c9eb895c3212f
fahmarosyidah <fahmarosyi@gmail.com>
2023-12-26 16:57:06 +0700

    initial commit

---
+++ app/src/main/java/com/example/consumerestapi/repository/KontakRepository.kt
@@ -0,0 +1,52 @@
+package com.example.consumerestapi.repository
+
+import com.example.consumerestapi.model.Kontak
+import com.example.consumerestapi.network.KontakService
+import java.io.IOException
+import java.lang.Exception
+
+interface KontakRepository {
+    /** Fetches List of Kontak from kontakApi*/
+    suspend fun  getKontak(): List<Kontak>
+
+    suspend fun insertKontak(kontak: Kontak)
+
+    suspend fun updateKontak(id: Int, kontak: Kontak)
+
+    suspend fun deleteKontak(id: Int)
+
+    suspend fun getKontakById(id: Int): Kontak
+}
+
+class NetworkKontakRepository(
+    private val kontakApiService: KontakService
+): KontakRepository {
+    /** Fetches List of Kontak from kontakApi*/
+    override suspend fun getKontak(): List<Kontak> = kontakApiService.getKontak()
+
+    override suspend fun insertKontak(kontak: Kontak) {
+        kontakApiService.insertKontak(kontak)
+    }
+
+    override suspend fun updateKontak(id: Int, kontak: Kontak) {
+        kontakApiService.updateKontak(id, kontak)
+    }
+
+    override suspend fun deleteKontak(id: Int) {
+        try {
+            val response = kontakApiService.deleteKontak(id)
+            if (!response.isSuccessful) {
+                throw IOException("Failed to delete kontak. HTTP status code: ${response.code()}")
+            }
+            else {
+                response.message()
+            }
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+
+    override suspend fun getKontakById(id: Int): Kontak {
+        return kontakApiService.getKontakById(id)
+    }
+}
---
+++ app/src/main/java/com/example/consumerestapi/ui/home/viewmodel/InsertViewModel.kt
@@ -0,0 +1,52 @@
+package com.example.consumerestapi.ui.home.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.consumerestapi.model.Kontak
+import com.example.consumerestapi.repository.KontakRepository
+
+class InsertViewModel(private val kontakRepository: KontakRepository) : ViewModel() {
+    var insertKontakState by mutableStateOf(InsertUiState())
+        private set
+
+    fun updateInsertKontakState(insertUiEvent: InsertUiEvent) {
+        insertKontakState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+
+    suspend fun insertKontak() {
+        viewModelScope
+    }
+}
+
+data class InsertUiEvent(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val nohp: String = ""
+)
+
+fun InsertUiEvent.toKontak() : Kontak = Kontak(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    nohp = nohp
+)
+
+data class InsertUiState(
+    val insertUiEvent: InsertUiEvent = InsertUiEvent()
+)
+
+fun Kontak.toInsertUiEvent(): InsertUiEvent = InsertUiEvent(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    nohp = nohp
+)
+
+fun Kontak.toUiStateKontak(): InsertUiState = InsertUiState(
+    insertUiEvent = toInsertUiEvent()
+)
+
