aa15694465ba005e6485c0436d345d30e2ac6b0f
fahmarosyidah <fahmarosyi@gmail.com>
2023-12-28 07:47:52 +0700

    menambahkan sealed class HomeScreen

---
+++ app/src/main/java/com/example/consumerestapi/ui/home/screen/HomeScreen.kt
@@ -0,0 +1,8 @@
+package com.example.consumerestapi.ui.home.screen
+
+import com.example.consumerestapi.navigation.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = "Kontak"
+}
